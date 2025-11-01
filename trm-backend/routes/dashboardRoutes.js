8e9ac619c1ef3e8e59a50889b1a839a2e659a782
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2025-11-02 00:19:28 +0545

    nice

--- trm-backend/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getDashboardStats } from "../controllers/DashboardController.js";
-import { checkAuthorization } from "../middleware/checkAuthorization.js";
-
-const router = express.Router();
-
-router.get("/stats", checkAuthorization, getDashboardStats);
-
-export default router;
